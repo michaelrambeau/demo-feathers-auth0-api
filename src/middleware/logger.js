2debad889b33202f725859910dbee2001ca449d8
michaelrambeau <mikeair@gmail.com>
2016-11-13 07:32:38 +0900

    Fix ESLint errors in 'src' folder

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,24 +1,23 @@
-'use strict';
+'use strict'
 
-const winston = require('winston');
+const winston = require('winston')
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Add a logger to our app object for convenience
-  app.logger = winston;
+  app.logger = winston
 
-  return function(error, req, res, next) {
+  return function (error, req, res, next) {
     if (error) {
-      const message = `${error.code ? `(${error.code}) ` : '' }Route: ${req.url} - ${error.message}`;
-      
+      const message = `${error.code ? `(${error.code}) ` : ''}Route: ${req.url} - ${error.message}`
+
       if (error.code === 404) {
-        winston.info(message);
-      }
-      else {
-        winston.error(message);
-        winston.info(error.stack);
+        winston.info(message)
+      } else {
+        winston.error(message)
+        winston.info(error.stack)
       }
     }
 
-    next(error);
-  };
-};
+    next(error)
+  }
+}
